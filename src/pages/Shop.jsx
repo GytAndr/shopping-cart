ff5576708072e888a4524b0b226e634fd38ee6a7
GytAndr <andriejunas@gmail.com>
2022-06-19 16:44:24 +0300

    category buttons filter

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -4,53 +4,44 @@ import '../Styles/Shop.css';
 const Shop = ({ add }) => {
 	const [products, setProducts] = useState([]);
 	const [isLoading, setIsLoading] = useState(true);
-	const categories = [
-		'smartphones',
-		'laptops',
-		'fragrances',
-		'skincare',
-		'groceries',
-		'home-decoration',
-		'furniture',
-		'tops',
-		'womens-dresses',
-		'womens-shoes',
-		'mens-shirts',
-		'mens-shoes',
-		'mens-watches',
-		'womens-watches',
-		'womens-bags',
-		'womens-jewellery',
-		'sunglasses',
-		'automotive',
-		'motorcycle',
-		'lighting',
-	];
-
+	const categories = [...new Set(products.map((product) => product.category))];
+	const filterItem = (curcat) => {
+		const newItem = products.filter((newVal) => {
+			return newVal.category === curcat;
+		});
+		setProducts(newItem);
+	};
+	async function FechProductList() {
+		const response = await fetch(
+			'https://dummyjson.com/products?limit=100&skip=0'
+		);
+		const json = await response.json();
+		//put response into state
+		setProducts(json.products);
+		//switch loading state
+		setIsLoading(false);
+	}
 	useEffect(() => {
-		async function FechProductList() {
-			const response = await fetch(
-				'https://dummyjson.com/products?limit=100&skip=0'
-			);
-			const json = await response.json();
-			//put response into state
-			setProducts(json.products);
-			//switch loading state
-			setIsLoading(false);
-		}
 		FechProductList();
 	}, []);
+
 	//if resposne was not received, show loading...
 	if (isLoading) {
 		return <p>Loading...</p>;
 	}
-	//else map trough array and display products.
 	return (
 		<div className="shop-container">
 			<div className="shop-categories">
+				<div className="category" onClick={() => FechProductList()}>
+					All categories
+				</div>
 				{categories.map((category, index) => {
 					return (
-						<div key={index} className="category">
+						<div
+							key={index}
+							className="category"
+							onClick={() => filterItem(category)}
+						>
 							{category}
 						</div>
 					);
