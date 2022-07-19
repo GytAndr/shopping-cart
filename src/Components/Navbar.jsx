6b8940cf82884f378de9b94d79749875478c471c
GytAndr <andriejunas@gmail.com>
2022-07-19 12:32:02 +0300

    Bootstrapped Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,4 @@
-import '../Styles/Navbar.css';
+// import '../Styles/Navbar.css';
 import { Link } from 'react-router-dom';
 import { FaShoppingCart } from 'react-icons/fa';
 
@@ -6,17 +6,43 @@ const Navbar = ({ cartProducts }) => {
 	const cart = cartProducts
 		.reduce((total, item) => item.qty + total, 0)
 		.toFixed(0);
+
 	return (
-		<nav>
-			<Link to="shopping-cart/">HOME</Link>
-			<Link to="shopping-cart/shop">SHOP</Link>
-			<Link to="shopping-cart/cart">
-				<div className="nav-cartbox">
-					<p>CART</p>
-					<FaShoppingCart id="nav-cart-icon" />
-					{cart == 0 ? null : <span id="nav-cart-number">{cart}</span>}
-				</div>
-			</Link>
+		<nav className="navbar navbar-expand-sm navbar-light bg-light mb-1">
+			<button
+				className="navbar-toggler ms-auto"
+				type="button"
+				data-bs-toggle="collapse"
+				data-bs-target="#navbarNav"
+			>
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div
+				className="collapse navbar-collapse justify-content-end me-3"
+				id="navbarNav"
+			>
+				<ul className="navbar-nav ">
+					<li className="nav-item ms-auto me-2">
+						<Link to="shopping-cart/" className="nav-link">
+							Home
+						</Link>
+					</li>
+					<li className="nav-item ms-auto me-2">
+						<Link to="shopping-cart/shop" className="nav-link">
+							Products
+						</Link>
+					</li>
+					<li className="nav-item ms-auto me-2 ">
+						<Link to="shopping-cart/cart" className="nav-link">
+							Cart
+							<FaShoppingCart className="ms-1" />
+							<span className="badge rounded-circle bg-danger ms-1">
+								{cart == 0 ? null : cart}
+							</span>
+						</Link>
+					</li>
+				</ul>
+			</div>
 		</nav>
 	);
 };
